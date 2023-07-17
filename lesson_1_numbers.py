f06a58c05d6bcb100bd98722194c4404b6b27f88
Pasola01 <pasolchik@gmail.com>
2023-07-17 21:16:55 +0300

    uncomment 1 numbers

--- lesson_1_numbers.py
+++ lesson_1_numbers.py
@@ -1,8 +1,8 @@
-# num_1 = 10.5
-# num_2 = 5.3
-# result = int(num_1 + num_2)
-#
-# print(result)
+num_1 = 10.5
+num_2 = 5.3
+result = int(num_1 + num_2)
+
+print(result)
 
 def sum_data(a, b):
     con = a + b
